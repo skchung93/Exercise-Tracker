e737c7682ace39f82b02aebe3125133ca11d2e37
Skchung93 <skchung93@gmail.com>
2021-10-21 21:35:21 -0700

    Trying to add the seed files and connection to the Mongo DB database

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 const db = require('../models');
 const Workout = require("../models/Workout.js");
+require('dotenv').config();
 
-mongoose.connect('mongodb://localhost/workout', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require('path');
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 
