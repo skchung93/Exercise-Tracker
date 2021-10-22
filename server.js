feec13da330d055d169a1b732785c22749193bf2
Skchung93 <skchung93@gmail.com>
2021-10-21 21:40:08 -0700

    added the dotenv to the package.json

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require('path');
-require('dotenv').config();
+
 
 const PORT = process.env.PORT || 3000;
 
